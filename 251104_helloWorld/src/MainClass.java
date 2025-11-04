d139e24822fcf20856e98e16062496610cf62395
DESKTOP-SA342QM\User <uee8351773@naver.com>
2025-11-04 17:32:10 +0900

    오늘 정규 수업 마지막 샘플 데이터
    시퀀스 \에 대한 정보
    
    오늘 시작 했으니가 지치지 말고 끝까지 해보자.

---
+++ 251104_helloWorld/src/MainClass.java
@@ -0,0 +1,19 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		System.out.print("hello world'");
+		
+
+	}
+
+}
+
+/*
+	\n 줄바꾸기
+	\b 백스페이스
+	\t 탭키 줄맞춤
+	\\ "\" 출력하기
+	\"   " " " 출력하기
+*/
