584867350e2bed6c18c92f69911a6ee2aee87615
BaeYoungWhan <uee8351773@naver.com>
2025-11-05 09:46:11 +0900

    첫째날 java  및 eclipse install url 삽입

--- 251104_helloWorld/src/MainClass.java
+++ 251104_helloWorld/src/MainClass.java
@@ -6,11 +6,17 @@ public class MainClass {
 		System.out.print("hello world'");
 		
 
-	}
+	} 
 
 }
 
-/*
+// JAVA 17 install 
+// https://www.oracle.com/java/technologies/javase/jdk17-archive-downloads.html 
+
+// Eclipse install
+// https://www.eclipse.org/downloads/packages/
+
+/*	
 	\n 줄바꾸기
 	\b 백스페이스
 	\t 탭키 줄맞춤
